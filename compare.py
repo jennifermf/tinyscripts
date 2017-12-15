edf67660fbbf4a4384d996a58f8c90ad5d5452c4
jmf aka ghost <jmfcodes@riseup.net>
2017-12-15 15:41:29 -0700

    added ability to compare case

--- compare.py
+++ compare.py
@@ -1,7 +1,12 @@
 # compare two strings
-print('------------------------\nIt looks like you want to compare two strings. I can help!\n(Comparison will ignore case.)\n')
-string1 = input('First string: ').lower()
-string2 = input('Second string: ').lower()
+print('------------------------\nIt looks like you want to compare two strings. I can help!\n')
+case = input('Would you like me to ignore case (A = a, B = b, etc)? [Y] for yes or any key for no: ')
+if case.lower() == 'y':
+	string1 = input('First string: ').lower()
+	string2 = input('Second string: ').lower()
+else: 
+	string1 = input('First string: ')
+	string2 = input('Second string: ')
 if string1 == string2: print('\n*TRUE!* These strings are identical.')
 else: print('\n*FALSE!* These strings are different.')
-print('\nRemember the command COMPARE to use me again. :)\n------------------------\n')
+print('\n------------------------\n')
